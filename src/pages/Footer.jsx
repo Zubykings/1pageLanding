a77321974fd58caddd168d122b534f0b66d9ced3
Zubykings <zubykings@gmail.com>
2023-07-12 13:03:39 +0100

    1Page Landing Page

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Footer extends Component {
   render() {
     return (
-      <div className="relative lg:h-[30vh] h-[20vh]  w-full bg-[#1660cf] text-white overflow-hidden flex flex-col items-center">
+      <div className="relative h-auto  w-full bg-[#1660cf] text-white overflow-hidden flex flex-col items-center">
         <div className="flex flex-row gap-3 w-[50%] justify-center px-5 m-auto  bottom-0 ">
           <img
             className="lg:w-[200px] w-[150px] h-[100px]  lg:h-[150px] rounded-ss-[60px] object-cover border-4 border-gray-500/50 shadow-2xl"
@@ -12,7 +12,7 @@ export default class Footer extends Component {
           />
 
           <div className="flex flex-col gap-1 text-start justify-end leading">
-            <h3 className="font-bold text-xl hover:underline w-40">6
+            <h3 className="font-bold text-xl hover:underline w-40">
               Zuby Kings
             </h3>
             <p className="font-medium text-sm lg:text-md">
